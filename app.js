feaf1e2ab211515e3e5f166cc6deef2bd35f4757
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-14 16:37:07 +0300

    feat: connect server with mongodb, use bodyParser and users router

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { router } = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
+
 const app = express();
 
-app.listen(PORT);
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/users', router);
+
+app.listen(PORT, () => {
+
+});
