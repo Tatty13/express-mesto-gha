d581aa835942864d7ed3ee7c386c64b4209d858f
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-15 17:34:56 +0300

    refactor: add default error to the handleError function

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,11 @@
+const CustomError = require('../errors/custom-error');
+
 function handleError(res, err, errDesc = 'Произошла ошибка') {
-  res.status(err.statusCode).send({ message: `${errDesc}: ${err.message}` });
+  if (err instanceof CustomError) {
+    res.status(err.statusCode).send({ message: `${errDesc}: ${err.message}` });
+  } else {
+    res.status(500).send({ message: `${errDesc}: ${err.message}` });
+  }
 }
 
 module.exports = {
