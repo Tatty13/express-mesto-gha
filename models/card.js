744262a9308f7e5c0c1a5d43c3e4ff985bdd3f97
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-15 19:45:41 +0300

    feat: add error description at models

--- models/card.js
+++ models/card.js
@@ -3,18 +3,18 @@ const mongoose = require('mongoose');
 const cardSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
+    required: [true, 'Имя не указано'],
+    minlength: [2, 'Длина имени не должна быть короче двух символов'],
+    maxlength: [30, 'Длина имени не должна превышать 30 символов'],
   },
   link: {
     type: String,
-    required: true,
+    required: [true, 'Ссылка не указана'],
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
-    required: true,
+    required: [true, 'Пользователь не указан'],
   },
   likes: {
     type: [mongoose.Schema.Types.ObjectId],
--- models/user.js
+++ models/user.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
+    required: [true, 'Имя не указано'],
+    minlength: [2, 'Длина имени не должна быть короче двух символов'],
+    maxlength: [30, 'Длина имени не должна превышать 30 символов'],
   },
   about: {
     type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
+    required: [true, 'Описание не указано'],
+    minlength: [2, 'Длина описания не должна быть короче двух символов'],
+    maxlength: [30, 'Длина описания не должна превышать 30 символов'],
   },
   avatar: {
     type: String,
-    required: true,
+    required: [true, 'Аватар не указан'],
   },
 });
 
