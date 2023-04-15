63f192f8ec46c263f39c32ea1f865a07ce20428e
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-15 19:48:22 +0300

    fix: handle mangoose validation errors: send error status 400 if error name is ValidationError

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,10 @@
 const CustomError = require('../errors/custom-error');
 
 function handleError(res, err, errDesc = 'Произошла ошибка') {
+  if (err.name === 'ValidationError') {
+    res.status(400).send({ message: `${errDesc}: ${err.message}` });
+    return;
+  }
   if (err instanceof CustomError) {
     res.status(err.statusCode).send({ message: `${errDesc}: ${err.message}` });
   } else {
