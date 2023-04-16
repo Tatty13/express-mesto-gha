d89a0a67046816401bacfcfaeb392ff25bd95f50
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-16 18:50:28 +0300

    refactor: change error descriptions

--- models/user.js
+++ models/user.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Имя не указано'],
+    required: [true, 'Имя пользователя не получено'],
     minlength: [2, 'Длина имени не должна быть короче двух символов'],
     maxlength: [30, 'Длина имени не должна превышать 30 символов'],
   },
   about: {
     type: String,
-    required: [true, 'Описание не указано'],
+    required: [true, 'Описание пользователя не получено'],
     minlength: [2, 'Длина описания не должна быть короче двух символов'],
     maxlength: [30, 'Длина описания не должна превышать 30 символов'],
   },
   avatar: {
     type: String,
-    required: [true, 'Аватар не указан'],
+    required: [true, 'Аватар пользователя не получен'],
   },
 });
 
