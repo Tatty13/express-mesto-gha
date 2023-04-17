86aae97caeb6df9933ac996bd0dc5bbc5bc9a78c
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-17 10:24:03 +0300

    refactor: uninstall body-parser, use express methods instead

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const router = require('./routes');
 
 const { PORT = 3000 } = process.env;
@@ -11,8 +10,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   req.user = {
