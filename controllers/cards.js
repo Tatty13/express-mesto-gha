5777a4aed539d87618e23a6f1538b20c64a94408
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-17 09:59:37 +0300

    refactor: populate owner and likes in getCards and deleteCards functions; move populate method right after findByIdAndUpdate method in putLike and deleteLike functions

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,10 +2,13 @@ const Card = require('../models/card');
 const NotFoundError = require('../errors/not-found-error');
 const { handleError } = require('../utils');
 
-function getCards(_, res) {
-  Card.find({})
-    .then((cards) => res.send(cards))
-    .catch((err) => handleError(res, err));
+async function getCards(_, res) {
+  try {
+    const cards = await Card.find({}).populate(['owner', 'likes']);
+    res.send(cards);
+  } catch (err) {
+    handleError(res, err);
+  }
 }
 
 async function createCard(req, res) {
@@ -25,7 +28,11 @@ async function deleteCard(req, res) {
   const { id } = req.params;
 
   try {
-    const card = await Card.findByIdAndRemove({ _id: id });
+    const card = await Card.findByIdAndRemove({ _id: id }).populate([
+      'owner',
+      'likes',
+    ]);
+
     if (!card) throw new NotFoundError('Карточка не найдена');
 
     res.send(card);
@@ -39,15 +46,14 @@ async function putLike(req, res) {
   const { _id: userId } = req.user;
 
   try {
-    const updatedCard = await Card.findByIdAndUpdate(
+    const card = await Card.findByIdAndUpdate(
       id,
       { $addToSet: { likes: userId } },
       { new: true },
-    );
+    ).populate(['owner', 'likes']);
 
-    if (!updatedCard) throw new NotFoundError('Карточка не найдена');
+    if (!card) throw new NotFoundError('Карточка не найдена');
 
-    const card = await updatedCard.populate(['owner', 'likes']);
     res.send(card);
   } catch (err) {
     handleError(res, err);
@@ -58,15 +64,14 @@ async function deleteLike(req, res) {
   const { id } = req.params;
   const { _id: userId } = req.user;
   try {
-    const updatedCard = await Card.findByIdAndUpdate(
+    const card = await Card.findByIdAndUpdate(
       id,
       { $pull: { likes: userId } },
       { new: true },
-    );
+    ).populate(['owner', 'likes']);
 
-    if (!updatedCard) throw new NotFoundError('Карточка не найдена');
+    if (!card) throw new NotFoundError('Карточка не найдена');
 
-    const card = await updatedCard.populate(['owner', 'likes']);
     res.send(card);
   } catch (err) {
     handleError(res, err);
