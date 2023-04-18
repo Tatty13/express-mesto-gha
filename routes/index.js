f5b40af928a208f085d6d02af433018fbcadf8ca
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-18 11:16:22 +0300

    refactorL use const as a status error to handle unknown routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
+const { NOT_FOUND_404 } = require('../utils/constants');
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
 router.use('*', (_, res) => {
-  res.status(404).send({ message: 'page not found' });
+  res.status(NOT_FOUND_404).send({ message: 'page not found' });
 });
 
 module.exports = router;
