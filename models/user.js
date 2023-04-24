41fe1a3f57872e735ebacc5264a7893cf7edcd35
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-24 21:13:14 +0300

    refactor: make name, about and avatar fields optional, set them default values

--- models/user.js
+++ models/user.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Имя пользователя не получено'],
+    default: 'Аноним',
     minlength: [2, 'Длина имени не должна быть короче двух символов'],
     maxlength: [30, 'Длина имени не должна превышать 30 символов'],
   },
   about: {
     type: String,
-    required: [true, 'Описание пользователя не получено'],
+    default: 'Остаюсь загадкой',
     minlength: [2, 'Длина описания не должна быть короче двух символов'],
     maxlength: [30, 'Длина описания не должна превышать 30 символов'],
   },
   avatar: {
     type: String,
-    required: [true, 'Аватар пользователя не получен'],
+    default: 'https://raw.githubusercontent.com/Tatty13/imgs-for-mesto-project/main/avatar.png',
   },
   email: {
     type: String,
