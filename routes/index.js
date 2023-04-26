0692867dd247a077e11ace81b66def4782d5ad19
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-26 14:13:55 +0300

    feat: add protect users and cards routes with auth middleware

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
 const router = require('express').Router();
+
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
+const auth = require('../middlewares/auth');
+
 const { login, createUser } = require('../controllers/users');
 const { NOT_FOUND_404 } = require('../utils/constants');
 
 router.post('/signin', login);
 router.post('/signup', createUser);
 
-router.use('/users', usersRouter);
-router.use('/cards', cardsRouter);
+router.use('/users', auth, usersRouter);
+router.use('/cards', auth, cardsRouter);
 router.use('*', (_, res) => {
   res.status(NOT_FOUND_404).send({ message: 'page not found' });
 });
