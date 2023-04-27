b77cbfd76007a48170c800db2eaa0e2b9943acdf
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-27 23:13:25 +0300

    feat: add joi validation to the routes with getUserById, updateUser, updateAvatar functions

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,12 @@ const {
   getUsers, getUserById, getUser, updateUser, updateAvatar,
 } = require('../controllers/users');
 
+const { validateUserId, validateUserInfo, validateUserAvatar } = require('../middlewares/validators/user-validators');
+
 usersRouter.get('/', getUsers);
 usersRouter.get('/me', getUser);
-usersRouter.get('/:id', getUserById);
-usersRouter.patch('/me', updateUser);
-usersRouter.patch('/me/avatar', updateAvatar);
+usersRouter.get('/:id', validateUserId, getUserById);
+usersRouter.patch('/me', validateUserInfo, updateUser);
+usersRouter.patch('/me/avatar', validateUserAvatar, updateAvatar);
 
 module.exports = usersRouter;
