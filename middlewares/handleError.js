55bf3d99f236b9cb612a98791fd94fa27a93a539
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-27 23:14:43 +0300

    feat: create handleError middleware

---
+++ middlewares/handleError.js
@@ -0,0 +1,56 @@
+const mongooseError = require('mongoose').Error;
+const { isCelebrateError } = require('celebrate');
+const { JsonWebTokenError } = require('jsonwebtoken');
+
+const CustomError = require('../errors/custom-error');
+
+const {
+  BAD_REQUEST_400,
+  UNAUTHORIZED_401,
+  CONFLICT_409,
+  INTERNAL_SERVER_ERROR_500,
+} = require('../utils/constants');
+
+function handleError(err, _, res, next) {
+  if (isCelebrateError(err)) {
+    const [errData] = err.details.values().next().value.details;
+    res.status(BAD_REQUEST_400).send({ message: errData.message });
+    return;
+  }
+
+  if (err instanceof mongooseError.ValidationError) {
+    const errMessage = Object.values(err.errors).map((e) => e.message).join('. ');
+    const resMessage = { message: errMessage };
+    res.status(BAD_REQUEST_400).send(resMessage);
+    return;
+  }
+
+  if (err instanceof mongooseError.CastError) {
+    const errMessage = `"${err.value}" incorrect. ${err.reason.message}`;
+    const resMessage = { message: errMessage };
+    res.status(BAD_REQUEST_400).send(resMessage);
+    return;
+  }
+
+  if (err.code === 11000) {
+    res.status(CONFLICT_409).send({ message: 'Пользователь с указанным email уже существует' });
+    return;
+  }
+
+  if (err instanceof JsonWebTokenError) {
+    res.status(UNAUTHORIZED_401).send({ message: 'Передан невалидный токен' });
+    return;
+  }
+
+  const resMessage = { message: err.message };
+
+  if (err instanceof CustomError) {
+    res.status(err.statusCode).send(resMessage);
+    return;
+  }
+
+  res.status(INTERNAL_SERVER_ERROR_500).send(resMessage);
+  next();
+}
+
+module.exports = handleError;
