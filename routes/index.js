cbbb42ffb40710d961d790f6b655ecd5722702e4
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-27 23:17:57 +0300

    feat: add handleError middleware to the end of routes; add joi validation to signin and signup routes

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,22 @@ const router = require('express').Router();
 
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
+
 const auth = require('../middlewares/auth');
+const handleError = require('../middlewares/handleError');
+const { validateUserCredential } = require('../middlewares/validators/user-validators');
 
 const { login, createUser } = require('../controllers/users');
 const { NOT_FOUND_404 } = require('../utils/constants');
 
-router.post('/signin', login);
-router.post('/signup', createUser);
+router.post('/signin', validateUserCredential, login);
+router.post('/signup', validateUserCredential, createUser);
 
 router.use('/users', auth, usersRouter);
 router.use('/cards', auth, cardsRouter);
 router.use('*', (_, res) => {
   res.status(NOT_FOUND_404).send({ message: 'page not found' });
 });
+router.use(handleError);
 
 module.exports = router;
