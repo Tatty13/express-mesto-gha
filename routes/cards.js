7b4b1a03195a9420bd08c8f579574e7a5c81ef6d
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-28 14:14:13 +0300

    feat: add joi card validators to the card routes

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,7 @@
 const cardsRouter = require('express').Router();
+
+const { validateCardData, validateCardId } = require('../middlewares/validators/card-validators');
+
 const {
   getCards,
   createCard,
@@ -8,9 +11,9 @@ const {
 } = require('../controllers/cards');
 
 cardsRouter.get('/', getCards);
-cardsRouter.post('/', createCard);
-cardsRouter.delete('/:id', deleteCard);
-cardsRouter.put('/:id/likes', putLike);
-cardsRouter.delete('/:id/likes', deleteLike);
+cardsRouter.post('/', validateCardData, createCard);
+cardsRouter.delete('/:id', validateCardId, deleteCard);
+cardsRouter.put('/:id/likes', validateCardId, putLike);
+cardsRouter.delete('/:id/likes', validateCardId, deleteLike);
 
 module.exports = cardsRouter;
