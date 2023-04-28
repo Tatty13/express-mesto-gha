429a542b07d1eb6f297f6e6935cbb7e19a1e1935
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-28 15:19:41 +0300

    feat: add custom link validation

--- models/card.js
+++ models/card.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const { urlPattern } = require('../utils/constants');
+
 const cardSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -10,6 +12,12 @@ const cardSchema = new mongoose.Schema({
   link: {
     type: String,
     required: [true, 'Ссылка на картинку не получена'],
+    validate: {
+      validator(value) {
+        return urlPattern.test(value);
+      },
+      message: 'Ссылка не валидна',
+    },
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
