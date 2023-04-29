2ac071ec97e679edddb1d930bf01390a3cd68abd
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-29 18:46:39 +0300

    refactor: create and use controller to handle unknown routes

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,24 @@ const { errors } = require('celebrate');
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 
+const handleUnknownRoute = require('../controllers/notFound');
 const auth = require('../middlewares/auth');
 const handleError = require('../middlewares/handleError');
 const { validateUserCredential } = require('../middlewares/validators/user-validators');
 
 const { login, createUser } = require('../controllers/users');
-const { NOT_FOUND_404 } = require('../utils/constants');
 
 router.post('/signin', validateUserCredential, login);
 router.post('/signup', validateUserCredential, createUser);
 
 router.use('/users', auth, usersRouter);
 router.use('/cards', auth, cardsRouter);
-router.use('*', (_, res) => {
-  res.status(NOT_FOUND_404).send({ message: 'page not found' });
-});
+router.use('*', auth, handleUnknownRoute);
+
+/**
+ * @todo remove line: router.use(errors())
+ * @todo use custom handleError middleware function to catch Joi errors
+ */
 router.use(errors()); // add line to pass github tests
 router.use(handleError);
 
