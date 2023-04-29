ce8455a78f41e60c23cf90d3118438ed99c6051a
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-29 19:01:32 +0300

    refactor: move error middlewares to the app

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
 
 const router = require('./routes');
 const { PORT, BD_URL, limiter } = require('./utils/config');
+const handleError = require('./middlewares/handleError');
 
 const app = express();
 
@@ -21,5 +23,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(router);
+
+/**
+ * @todo remove line: router.use(errors())
+ * @todo use custom handleError middleware function to catch Joi errors
+ */
+app.use(errors()); // add line to pass github tests
+app.use(handleError);
 
 app.listen(PORT, () => {});
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 const router = require('express').Router();
-const { errors } = require('celebrate');
 
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 
 const handleUnknownRoute = require('../controllers/notFound');
 const auth = require('../middlewares/auth');
-const handleError = require('../middlewares/handleError');
 const { validateUserCredential } = require('../middlewares/validators/user-validators');
 
 const { login, createUser } = require('../controllers/users');
@@ -17,12 +15,5 @@ router.post('/signup', validateUserCredential, createUser);
 router.use('/users', auth, usersRouter);
 router.use('/cards', auth, cardsRouter);
 router.use('*', auth, handleUnknownRoute);
-
-/**
- * @todo remove line: router.use(errors())
- * @todo use custom handleError middleware function to catch Joi errors
- */
-router.use(errors()); // add line to pass github tests
-router.use(handleError);
 
 module.exports = router;
