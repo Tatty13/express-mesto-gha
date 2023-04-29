6a911160574013e3072590bcada0628e74840f67
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-29 18:17:29 +0300

    fix: make name, about and avatar keys required in user alidation on data update

--- middlewares/validators/user-validators.js
+++ middlewares/validators/user-validators.js
@@ -20,14 +20,14 @@ const validateUserCredential = celebrate({
 
 const validateUserInfo = celebrate({
   body: Joi.object({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30).required(),
   }),
 });
 
 const validateUserAvatar = celebrate({
   body: Joi.object({
-    avatar: Joi.string().pattern(urlPattern),
+    avatar: Joi.string().pattern(urlPattern).required(),
   }),
 });
 
