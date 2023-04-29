773f0b3f672e69adafb39d079fc5636151493e0f
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-29 14:01:16 +0300

    feat: use helmet in the app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
 const router = require('./routes');
@@ -14,6 +15,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
