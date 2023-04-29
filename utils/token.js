9bf1dd7dd4839a6d5370f3e433b4a1da58431b6d
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-29 16:25:06 +0300

    refactor: add jsdoc commenta to the token.js file

--- utils/token.js
+++ utils/token.js
@@ -4,8 +4,20 @@ const { jwtSecter } = require('./constants');
 
 const { JWT_SECRET = jwtSecter } = process.env;
 
+/**
+ * @typedef {(String | Object | Buffer)} Payload
+ */
+
+/**
+ * @param {Payload} payload
+ * @returns {string} token
+ */
 const generateToken = (payload) => jwt.sign(payload, JWT_SECRET, { expiresIn: '7d' });
 
+/**
+ * @param {string} token
+ * @returns { Payload | Error} token payload or Error
+ */
 const verifyToken = (token) => jwt.verify(token, JWT_SECRET);
 
 module.exports = {
